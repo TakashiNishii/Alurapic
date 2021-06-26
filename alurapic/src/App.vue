8459527613ab3509d7c7a7b336e742456376df3e
Takashi <ygortn14@gmail.com>
2021-06-26 18:50:57 -0400

    :rocket: Conceito de Single Page Aplication, VueRouter, criação de rotas, modo history e hash

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,60 +1,23 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input
-      type="search"
-      class="filtro"
-      @input="filtro = $event.target.value"
-      placeholder="Filtre por parte do título"
-    />
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo">
-          </imagem-responsiva>
-        </meu-painel>
-      </li>
-    </ul>
+    <meu-menu :rotas="routes" />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel.vue";
-import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
-
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
 export default {
   components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    "meu-menu": Menu
   },
   data() {
     return {
-      titulo: "Alurapic",
-      fotos: [],
-      filtro: ""
+      routes
     };
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        /*Filtrar*/
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(
-        fotos => (this.fotos = fotos),
-        err => console.log(err)
-      );
   }
 };
 </script>
@@ -65,21 +28,12 @@ export default {
   width: 96%;
   margin: 0 auto;
 }
-
-.centralizado {
-  text-align: center;
+.pagina-enter,
+.pagina-fade-leave-active {
+  opacity: 0;
 }
-
-.lista-fotos {
-  list-style: none;
-}
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.4s;
 }
 </style>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+//Esse tem nome diferente por não ser export default que temos em routes.js
+import {routes} from './routes';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+//Crio um roteador para ser usado na aplicação:
+const router = new VueRouter({ 
+  routes,
+  mode: 'history' 
+});
+
 
 new Vue({
   el: '#app',
+  router, //Por ter o mesmo nome posso colocar só um no lugar de `router:router,`
   render: h => h(App)
 })
