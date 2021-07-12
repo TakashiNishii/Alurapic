f4abb436e198960a2e04e34ed6ab5296bde65616
Takashi <ygortn14@gmail.com>
2021-07-12 18:56:36 -0400

    :arrow_up: Conceito sobre o uso do VeeValidate (porém adaptei o código para a versão mais atual inves do beta 2.0)

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -6,38 +6,63 @@
     <h2 v-if="foto._id" class="centralizado">Alterando</h2>
     <h2 v-else class="centralizado">Incluindo</h2>
 
-    <form @submit.prevent="grava()">
-      <div class="controle">
-        <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo" />
-      </div>
+    <validationObserver v-slot="{ handleSubmit }">
+      <form @submit.prevent="grava()">
+        <div class="controle">
+          <label for="titulo">TÍTULO</label>
+          <validation-provider
+            rules="required|minmax:3,20"
+            :bails="false"
+            v-slot="{ errors }"
+          >
+            <input
+              v-model="foto.titulo"
+              id="titulo"
+              vid="titulo"
+              name="título"
+              autocomplete="off"
+              style="width: 100%"
+            />
+            <span class="erro">{{ errors[0] }}</span>
+          </validation-provider>
+        </div>
 
-      <div class="controle">
-        <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url" />
-        <imagem-responsiva
-          v-show="foto.url"
-          :url="foto.url"
-          :titulo="foto.titulo"
-        />
-      </div>
+        <div class="controle">
+          <label for="url">URL</label>
+          <validation-provider rules="required" v-slot="{ errors }">
+            <input
+              v-model.lazy="foto.url"
+              id="url"
+              autocomplete="off"
+              name="url"
+              style="width: 100%"
+            />
+            <span class="erro">{{ errors[0] }}</span>
+          </validation-provider>
+          <imagem-responsiva
+            v-show="foto.url"
+            :url="foto.url"
+            :titulo="foto.titulo"
+          />
+        </div>
 
-      <div class="controle">
-        <label for="descricao">DESCRIÇÃO</label>
-        <textarea
-          id="descricao"
-          autocomplete="off"
-          v-model="foto.descricao"
-        ></textarea>
-      </div>
+        <div class="controle">
+          <label for="descricao">DESCRIÇÃO</label>
+          <textarea
+            id="descricao"
+            autocomplete="off"
+            v-model="foto.descricao"
+          ></textarea>
+        </div>
 
-      <div class="centralizado">
-        <meu-botao rotulo="GRAVAR" tipo="submit" />
-        <router-link :to="{ name: 'home' }"
-          ><meu-botao rotulo="VOLTAR" tipo="button"
-        /></router-link>
-      </div>
-    </form>
+        <div class="centralizado">
+          <meu-botao rotulo="GRAVAR" tipo="submit" />
+          <router-link :to="{ name: 'home' }"
+            ><meu-botao rotulo="VOLTAR" tipo="button"
+          /></router-link>
+        </div>
+      </form>
+    </validationObserver>
   </div>
 </template>
 
@@ -62,11 +87,13 @@ export default {
 
   methods: {
     grava() {
-      //Enviar dado
       this.service.cadastra(this.foto).then(
         () => {
-          if (this.id) this.$router.push({ name: "home" });
-          this.foto = new Foto();
+          if (this.id)
+            //se tem id, é pq tratou-se de um update, então, após a gravação, retorne p/ tela inicial
+            this.$router.push({ name: "home" }); //O VueRouter disponibiliza no global view instance o objeto $router que possui o push. serve p/ redirecionar p/ alguma pagina
+
+          this.foto = new Foto(); //caso o if de cima for falso, apenas os campos da tela serão zerados e o usuario permanece na pagina.
         },
         err => console.log(err)
       );
@@ -104,5 +131,9 @@ export default {
 
 .centralizado {
   text-align: center;
+}
+
+.erro {
+  color: red;
 }
 </style>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -6,6 +6,7 @@ import VueRouter from 'vue-router';
 import {routes} from './routes';
 
 import './directives/Transform';
+import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
@@ -19,6 +20,29 @@ const router = new VueRouter({
   mode: 'history' 
 });
 
+
+extend('required', {
+  validate (value) {
+    return {
+      required: true,
+      valid: ['', null, undefined].indexOf(value) === -1
+    };
+  },
+  computesRequired: true,
+  message: 'Este campo é obrigatório!'
+});
+
+extend('minmax', {
+  validate(value, { min, max }) {
+    return value.length >= min && value.length <= max;
+  },
+  params: ['min', 'max'],
+  message:'O campo {_field_} deve possuir no mínimo {min} e no máximo {max} caracteres.'
+
+});
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 new Vue({
   el: '#app',
